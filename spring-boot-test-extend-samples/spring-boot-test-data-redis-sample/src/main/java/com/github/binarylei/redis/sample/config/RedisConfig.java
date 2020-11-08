dbb87c91f5fb1d2ade0be37aaf3977fe06bf219b
binarylei <binarylei@qq.com>
2020-11-08 10:24:11 +0800

    【redis】添加sample案例

---
+++ spring-boot-test-extend-samples/spring-boot-test-data-redis-sample/src/main/java/com/github/binarylei/redis/sample/config/RedisConfig.java
@@ -0,0 +1,13 @@
+package com.github.binarylei.redis.sample.config;
+
+import com.github.binarylei.redis.configuration.AutoConfigureTestRedis;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author binarylei
+ * @version 2020-11-08
+ */
+@Configuration
+@AutoConfigureTestRedis
+public class RedisConfig {
+}
